1ad832ec65a39695a5159ac1073f39d4f317fdd6
Pekka Onnela <pekka.onnela@onrego.fi>
2021-03-26 18:25:00 +0200

    Add files via upload

---
+++ ARMWebhookScript.ps1
@@ -0,0 +1,10 @@
+﻿param([string] $VMName,[string] $StaticMemory,[string] $ProcessorCount,[string] $Image,[string] $Location)
+$uri = 'https://f3b60840-5f07-4bfb-82e7-10723d26c506.webhook.we.azure-automation.net/webhooks?token=vYfMXFirXJkyDSPjhk873Iayt6bFr8KWcih5gxbH5UU%3d'
+$params  = @(@{ VMName=$VMName;StaticMemory=$StaticMemory;ProcessorCount=$ProcessorCount;Image=$Image})
+$body = ConvertTo-Json -InputObject $params
+$header = @{ message='StartedbyOnrego'}
+$response = Invoke-WebRequest -Method Post -Uri $uri -Body $body -Headers $header
+$output = (ConvertFrom-Json ($response.Content)).jobids[0]
+Write-Output $output
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['text'] = $output
